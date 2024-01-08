664187af6b435f8d0ade747c198abfda4e03833b
srikanth65 <srikanthch0465@gmail.com>
2024-01-08 11:15:34 -0800

    remote state

--- terraform_robo/providers.tf
+++ terraform_robo/providers.tf
@@ -5,8 +5,16 @@ terraform {
       version = "5.31.0"
     }
   }
+  backend "s3" {
+    bucket         = "terraform-tfstate-sri"
+    key            = "terraform_robo"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_robo"
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
+
